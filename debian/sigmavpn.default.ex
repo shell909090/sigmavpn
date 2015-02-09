59679810cc82b0df6a1b0c7f12574da9187a10d9
Shell.Xu <shell909090@gmail.com>
2015-02-10 02:28:31 +0800

    * inital debian package.

---
+++ debian/sigmavpn.default.ex
@@ -0,0 +1,10 @@
+# Defaults for sigmavpn initscript
+# sourced by /etc/init.d/sigmavpn
+# installed at /etc/default/sigmavpn by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
