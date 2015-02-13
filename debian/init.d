9d209b1ef8ae1b2aae3bf40630cafefecc78bf95
Shell.Xu <shell909090@gmail.com>
2015-02-13 11:47:22 +0800

    * version issue * copyright issue + description in init.d + diff header

--- debian/init.d
+++ debian/init.d
@@ -6,6 +6,7 @@
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
 # Short-Description: light-weight VPN providing easy-to-configure and secure VPN tunnels
+# Description:       SigmaVPN is a light-weight VPN solution aimed at providing easy-to-configure and secure VPN tunnels for UNIX systems.
 ### END INIT INFO
 
 # Author: Shell.Xu <shell909090@gmail.com>
